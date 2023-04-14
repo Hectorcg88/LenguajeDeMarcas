e056214406d5d69339ad2e9e07e3a66440a6ae0a
Hectorcg88 <casesgimenezh@gmail.com>
2023-04-14 17:03:15 +0200

    Acabado pagina prductos
    Pagina de productos terminada y comentado codigo de js

--- js/navegation.js
+++ js/navegation.js
@@ -1,16 +1,29 @@
+/**
+ * Con esta funcion aparece un mensaje en pantalla al darle al boton deseado para indicar que todo ha fincionado correctamente
+ */
+
 function hola(){
   swal("Gracias jefe!", "Te responderé en cuanto pueda", "success");
 }
 
+
+/**
+ * Declaración de constantes para hacer los menus
+ */
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
-
+/**
+ * Funcion para desplegar el menu
+ */
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
 })
 
+/**
+ * Funcion para cerrar el menu
+ */
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
